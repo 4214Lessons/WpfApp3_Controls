dc50ae658792fabdca7b03deeb8554aa2b6b5f6e
Tural Novruzov <t.novruzov@itstep.edu.az>
2022-10-21 17:58:20 +0400

    last commit

--- Source/MainWindow.xaml.cs
+++ Source/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
+using System.Windows.Controls.Ribbon;
 
 namespace Source;
 
@@ -11,6 +12,14 @@ public partial class MainWindow : Window
     {
         InitializeComponent();
         // frame.Source = new Uri("https://www.google.com/");
+
+
+
+        // Button
+        // RepeatButton
+        // ToggleButton
+        // CheckBox
+        // RadioButton
     }
 
     private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -35,8 +44,38 @@ public partial class MainWindow : Window
     {
         //if (lbl is not null)
         //{
-            //var slider = sender as Slider;
-            //lbl.FontSize = slider?.Value ?? 10;
+        //var slider = sender as Slider;
+        //lbl.FontSize = slider?.Value ?? 10;
         //}
+    }
+
+
+
+    // int counter = 0;
+    // private void Button_Click(object sender, RoutedEventArgs e)
+    // {
+    //     textBlock.Text = counter++.ToString();
+    // }
+
+
+
+    private void RepeatButton_Click(object sender, RoutedEventArgs e)
+    {
+        //if (sender is RepeatButton btn)
+        //{
+        //    progressBar.Value = (btn.HorizontalAlignment == HorizontalAlignment.Left)
+        //        ? --progressBar.Value
+        //        : ++progressBar.Value;
+        //}
+    }
+
+    private void ToggleButton_Checked(object sender, RoutedEventArgs e)
+    {
+        MessageBox.Show((sender as ToggleButton)?.IsChecked.ToString());
+    }
+
+    private void ToggleButton_Unchecked(object sender, RoutedEventArgs e)
+    {
+        MessageBox.Show((sender as ToggleButton)?.IsChecked.ToString());
     }
 }
